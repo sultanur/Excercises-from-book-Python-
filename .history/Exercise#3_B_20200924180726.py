3198f6cd34a4d2aa20ed0cab8ca14bb3da8f478c
Nuray Sultan <nursultann81@gmail.com>
2020-09-24 18:23:46 +0200

    Commotting

---
+++ .history/Exercise#3_B_20200924180726.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def place_floats (ffl, int1, int2): 
+    to_str = str(ffl)
+    indexx = to_str.index('.')
+    return to_str[indexx - before:indexx+after+1]
+printf(f'{place_floats (1234.5678, 2,3)}')
+    
---
+++ .history/Exercise#3_B_20200924180756.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def place_floats (ffl, int1, int2): 
+    to_str = str(ffl)
+    indexx = to_str.index('.')
+    return to_str[indexx - before:indexx+after+1]
+
+print(f'{place_floats (1234.5678, 2,3)}')
+    
