62ed75d19d60a7e0f956dc64c8cb8799feebe947
Nuray Sultan <nursultann81@gmail.com>
2020-09-26 21:21:22 +0200

    Committing the last changes on task related to strings

---
+++ .history/Exercise#5_D_20200926210954.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import string
+
+def punctuation(my_word): 
+    punctuation = ''
+    if my_word[-1] in '!.:,?':
+        punctuation = my_word[-1]
+        my_word = my_word[:-1] 
+           
+    if my_word[0] in 'aeiou': 
+        result = f'{my_word} way'+punctuation
+    else: 
+        result = f'{my_word[1:]}{my_word[0]}ay'+punctuation
+        
+    return result   
+    
+         
+print(punctuation('wine!'))
