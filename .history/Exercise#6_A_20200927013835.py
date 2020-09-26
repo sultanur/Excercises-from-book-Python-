75ee2a7df35403e633cbd8f195c3035959a39092
Nuray Sultan <nursultann81@gmail.com>
2020-09-27 01:39:03 +0200

    Committting task #6_B

---
+++ .history/Exercise#6_A_20200927013835.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+
+def pl_sentence (sentence): 
+    output = []
+    punctuation = ''
+    for word in sentence.split():
+        n = False
+        if word[-1] in '?!.,':
+            punctuation = word[-1] 
+            word = word[:-1]
+            n == True  
+            if word[0] in 'aeiou':
+                output.append(f'{word}way' + punctuation)
+            else:
+                output.append(f'{word[1:]}{word[0]}ay' + punctuation)
+        
+        elif word[0] in 'aeiou':
+            output.append(f'{word}way')     
+        else:
+            output.append(f'{word[1:]}{word[0]}ay')
+            
+    
+    return ' '.join(output)
+   
+
+
+print(pl_sentence("Hi Nuray, How was your day?"))        
