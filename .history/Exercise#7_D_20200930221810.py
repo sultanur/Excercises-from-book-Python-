84092ce96035e48c012b754d2307dbca3ceeb3f3
Nuray Sultan <nursultann81@gmail.com>
2020-09-30 22:19:07 +0200

    Committing task#7_D

---
+++ .history/Exercise#7_D_20200930221810.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import string
+def URL_encode_chars(url_encoded_str):
+    output = []
+    stand_word = string.ascii_letters+ string.digits
+    for one_char in url_encoded_str: 
+        if one_char in stand_word:   
+            output.append(one_char)
+        else:   
+            output.append(hex(ord(one_char)).replace('0x', '%20'))  
+            
+    return ''.join(output)
+
+print(URL_encode_chars('https://www.w3schools.com/python/ref_string_split.asp'))
+           
+    
