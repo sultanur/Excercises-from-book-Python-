645656b1e8982a6f4b257af2ef9aad7a924d69ae
Nuray Sultan <nursultann81@gmail.com>
2020-10-02 23:19:32 +0200

    comitting task on finding (sum diff sum diff etc) sequence of elements

---
+++ .history/Exercise#9_C_20201002225106.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def even_odd_sums(numbers):
+    for num, i in enumerate(numbers):
+        res = i + i
+        print(i ,' on ', num)
+        print(res)        
+   
+#print(even_odd_sums((10, 20, 30, 40, 50, 60)))
+print(even_odd_sums([10, 20, 30, 40, 50, 60]))
---
+++ Exercise#9_C.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def even_odd_diff(numbers):
+    odds = (numbers[1::2])        
+    evens =(numbers[2::2])
+    res = numbers[0] + sum(odds) - sum(evens) 
+    return res    
+   
+print(even_odd_diff((10, 200, 30, 40, 50, 60)))
+print(even_odd_diff([10, 20, 30, 40, 50, 60]))
+
+    
