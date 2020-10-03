9cad61a0ff4821c9f3c89246b6d0fb4ddc48df4c
Nuray Sultan <nursultann81@gmail.com>
2020-10-03 15:33:22 +0200

    Committing task (fun for summing numbers bigger than the 1st element)

---
+++ .history/Exercise#10_B_20201003152602.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def mysum_bigger_than(*items):  
+    if not items:   
+        return items   
+    output = items[0]
+    for item in items[1:]:   
+        if items[0] <= item: 
+            output +=item
+        else:   
+            continue
+    return output
+    
+print(mysum_bigger_than(5, 5, 20, 30, 60)) 
+        
